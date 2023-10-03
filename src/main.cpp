cb139180966e3f2cc0dfaa812372f33cda5e84fb
Griswald Brooks <griswald.brooks@gmail.com>
2023-10-03 06:40:56 +0000

    Working demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,52 @@
 #include <iostream>
+#include <string>
 #include "serial/serial.h"
 #include "mdspan.hpp"
 
 namespace stdex = std::experimental;
 
-template <typename T, std::ptrdiff_t Width, std::ptrdiff_t Height>
-struct robot_command_accessor {
-    using data_handle_type = T*;
-    using reference = T&;
-    using element_type = T;
-
-    T& access(data_handle_type ptr, std::ptrdiff_t offset) const {
-        std::cout << "did access\n";
-        std::cin >> ptr[offset];
-        return ptr[offset];
+struct robot_arm {
+  bool is_bin_occupied(int bin) const {
+    if (!robot_arm_.isOpen()) {
+      std::cout << "Error opening serial port\n";
+      return false;
     }
-    
-    std::pair<std::ptrdiff_t, std::ptrdiff_t> offset_to_coord(std::ptrdiff_t offset) const {
-        std::ptrdiff_t row = offset / Width;
-        std::ptrdiff_t col = offset % Width;
-        return {row, col};
-    }
-
-    void send_robot_command(std::ptrdiff_t row, std::ptrdiff_t col) const {
-        std::cout << "Sending robot to position corresponding to pixel (" << row << ", " << col << ")\n";
-    }
+    auto const bin_str = std::to_string(bin) + "\n";
+    robot_arm_.write(bin_str);
+    // TODO wrap in try catch and return unexpected
+    auto const result = std::stoi(robot_arm_.readline());
+    return result == 1;
+  }
+  
+  mutable serial::Serial robot_arm_ = serial::Serial("/dev/ttyACM1", 9600, serial::Timeout::simpleTimeout(10000));
 };
 
+// TODO Covert this to an enum
+struct bin_state {
+  bool is_occupied;
+};
+
+template <int nbins>
+struct robot_command_accessor {
+  using element_type = bin_state;
+  using reference = bin_state&;
+  using data_handle_type = bin_state*;
+
+    reference access(data_handle_type ptr, std::ptrdiff_t offset) const {
+    // TODO: check nbins and return UNKNOWN enum
+      auto const arm = robot_arm{};
+      ptr[offset].is_occupied = arm.is_bin_occupied(static_cast<int>(offset));
+      return ptr[offset];
+    }
+};
+ using ext_t = stdex::extents<uint32_t, 4>;
+ using acc_t = robot_command_accessor<4>;
+
 int main(int, char **) {
-  // port, baudrate, timeout in milliseconds
-  auto robot_arm = serial::Serial("/dev/ttyACM0", 9600, serial::Timeout::simpleTimeout(1000));
-  if(!robot_arm.isOpen()) {
-    std::cout << "Error opening serial port\n";
-    return -1;
+  auto bin_array = std::array<bin_state, 4>{};
+  auto bins = stdex::mdspan<bool, ext_t, stdex::layout_right, acc_t>(bin_array.data());
+  for (auto ndx = 0; ndx < 4; ++ndx) {
+    std::cout << "bin[" << ndx << "] is occupied: " << (bins(ndx).is_occupied ? "yes" : "no") << "\n";
   }
-
-  size_t bytes_wrote = robot_arm.write("0");
-  std::cout << "Wrote " << bytes_wrote << " bytes" << std::endl;
-  auto const result = robot_arm.read(2);
-  std::cout << result;
-
-  static constexpr int width = 100;
-  std::vector<int> image_data(width * width);
-  using ext_t = stdex::extents<uint32_t, width, width>;
-  using acc_t = robot_command_accessor<int, width, width>;
-  stdex::mdspan<int, ext_t, stdex::layout_right, acc_t> image_view(image_data.data());
-  // robot arm should fetch the value for the access
-  std::cout << "image_view[50, 50] = " << image_view(50, 50) << "\n";
-  image_view(50, 50) = 1;
-  std::cout << "image_view[50, 50] = " << image_view(50, 50) << "\n";
-
   return 0;
 }
