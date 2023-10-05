5f0b42cb304cc0ae25f6ff1ac8da78e83bd01cfc
Griswald Brooks <griswald.brooks@gmail.com>
2023-10-05 07:51:25 +0000

    First version of talk and cleanup cache

--- presentation/talk.py
+++ presentation/talk.py
@@ -41,7 +41,6 @@ def code_slide(parent: Box, title: str, language: str, code: str):
         language, code
     )
 
-
 @slides.slide(debug_boxes=False)
 def title(slide):
     content = logo_header_slide(slide, "")
@@ -58,31 +57,129 @@ def title(slide):
 def normal_usage(slide):
     code_slide(
         slide,
+        "mdspan",
+        "C++",
+        """
+// Allocate the map
+std::array map_storage{...};
+// Create a 2D view of the map
+auto occupancy_map = std::mdspan(map_storage.data(), 384, 384);
+// Update the map 
+for (std::size_t i = 0; i != occupancy_map.extent(0); i++) {
+    for (std::size_t j = 0; j != occupancy_map.extent(1); j++) {
+        occupancy_map[i, j] = get_occupancy_from_laser_scan(...)
+    }
+}
+// TODO: Add turtlebot map
+""",
+    )
+    slide.set_style("footer", elsie.TextStyle(color="black", size=32, align="right"))
+    footer = slide.box(width="fill", height="5%", horizontal=True)
+    f1 = footer.box(width="fill", height="fill")
+    f1.text("github.com/griswaldbrooks/spanny", "footer")
+
+
+@slides.slide(debug_boxes=False)
+def policy_injection(slide):
+    code_slide(
+        slide,
+        "Policy Injection",
+        "C++",
+        """
+template <
+  class T,
+  class Extents,
+  class LayoutPolicy = std::layout_right,
+  class Accessor = std::default_accessor
+>
+class mdspan;
+""",
+    )
+    slide.set_style("footer", elsie.TextStyle(color="black", size=32, align="right"))
+    footer = slide.box(width="fill", height="5%", horizontal=True)
+    f1 = footer.box(width="fill", height="fill")
+    f1.text("github.com/griswaldbrooks/spanny", "footer")
+
+
+@slides.slide(debug_boxes=False)
+def bin_accessor(slide):
+    code_slide(
+        slide,
+        "Bin Accessor Policy",
+        "C++",
+        """
+template <int nbins>
+struct bin_checker {
+  using element_type = tl::expected<bin_state, std::string>;
+  using reference = tl::expected<bin_state, std::string> const&;
+  using data_handle_type = robot_arm*;
+
+  inline static element_type const no_bin = tl::make_unexpected("Invalid bin");
+  mutable element_type recent_ = tl::make_unexpected("Bin has not been accessed");
+
+  reference access(data_handle_type arm, std::ptrdiff_t offset) const {
+    // Return an error if the offset is out of bounds
+    if (offset < 0 || offset >= nbins) return no_bin;
+    // Set the bin state in a member of the accessor policy
+    recent_ = arm->is_bin_occupied(static_cast<int>(offset));
+    // This is because we return a reference
+    return recent_;
+  }
+};
+""",
+    )
+    slide.set_style("footer", elsie.TextStyle(color="black", size=32, align="right"))
+    footer = slide.box(width="fill", height="5%", horizontal=True)
+    f1 = footer.box(width="fill", height="fill")
+    f1.text("github.com/griswaldbrooks/spanny", "footer")
+
+
+@slides.slide(debug_boxes=False)
+def async_implementation(slide):
+    code_slide(
+        slide,
         "Getting Started",
         "C++",
         """
-int main(int argc, char ** argv)
-{
-  rclcpp::init(argc, argv);
+using ext_t = stdex::extents<uint32_t, 2, 3>;
+using acc_t = bin_checker<6>;
+using bin_view = stdex::mdspan<bin_state, ext_t, stdex::layout_right, acc_t>;
 
-  auto node = std::make_shared<rclcpp::Node>("minimal_param_node");
-  auto my_string = node->declare_parameter("my_string", "world");
-  auto my_number = node->declare_parameter("my_number", 23);
-
-  rclcpp::spin(node);
-  rclcpp::shutdown();
+int main(int, char **) {
+  auto arm = robot_arm{"/dev/ttyACM0", 9600};
+  auto bins = bin_view(&arm, {}, bin_checker<6>{});
+  while(true) {
+    for (std::size_t i = 0; i != bins.extent(0); ++i) {
+      for (std::size_t j = 0; j != bins.extent(1); ++j) {
+        std::cout << "Bin " << i << ", " << j << " is ";
+        bins(i, j).and_then(print_state).or_else(shrug);
+        std::cout << "\\n";
+      }
+    }
+    std::cout << "====================\\n";
+  }
+  return 0;
 }
 """,
     )
+    slide.set_style("footer", elsie.TextStyle(color="black", size=32, align="right"))
+    footer = slide.box(width="fill", height="5%", horizontal=True)
+    f1 = footer.box(width="fill", height="fill")
+    f1.text("github.com/griswaldbrooks/spanny", "footer")
+
 
 @slides.slide(debug_boxes=False)
 def demo_time(slide):
     content = logo_header_slide(slide, "")
-    content.box().text("30 lines of C++ boilderpate per parameter")
-    content.box(show="2").text(
-        "Before handling of dynamic parameters", elsie.TextStyle(color="red")
+    content.box(width="fill").text(
+        "Demo Time", elsie.TextStyle(size=160, bold=True)
     )
-
+    
+    slide.set_style("footer", elsie.TextStyle(color="black", size=32, align="right"))
+    footer = slide.box(width="fill", height="5%", horizontal=True)
+    f1 = footer.box(width="fill", height="fill")
+    f1.text("github.com/griswaldbrooks/spanny", "footer")
+    
 @slides.slide(debug_boxes=False)
 def thank_you(slide):
     content = logo_header_slide(slide, "")
