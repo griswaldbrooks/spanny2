f94f3ce62bd94dfb0626e4f9bb6677724c3584ed
Griswald Brooks <griswald.brooks@gmail.com>
2024-09-08 11:21:01 -0400

    Add windows from layout stride, submdspan, rotatable, polygonal, and save/load from image (#1)

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
-#include <iostream>
+#include "mdspan.hpp"
+#include "serial/serial.h"
 #include <future>
+#include <iostream>
 #include <memory>
 #include <stdexcept>
 #include <string>
 #include <vector>
-#include "serial/serial.h"
-#include "mdspan.hpp"
 
 namespace stdex = std::experimental;
 
-enum struct bin_occupancy {
-  OCCUPIED,
-  EMPTY
-};
+enum struct bin_occupancy { OCCUPIED, EMPTY };
 
 struct bin_coordinate {
   int x, y;
@@ -23,19 +20,16 @@ struct bin_state {
   bin_coordinate coordinate;
 };
 
-
 bin_coordinate offset_to_coord(int offset, int width = 3) {
-    int const row = offset / width;
-    int const col = offset % width;
-    return {row, col};
+  int const row = offset / width;
+  int const col = offset % width;
+  return {row, col};
 }
 
 struct robot_arm {
-  robot_arm(
-    std::string port,
-    uint32_t baudrate,
-    serial::Timeout timeout = serial::Timeout::simpleTimeout(10000))
-    : serial_(std::move(port), baudrate, std::move(timeout)) {}
+  robot_arm(std::string port, uint32_t baudrate,
+            serial::Timeout timeout = serial::Timeout::simpleTimeout(10000))
+      : serial_(std::move(port), baudrate, std::move(timeout)) {}
 
   bin_occupancy is_bin_occupied(int bin) {
     if (!serial_.isOpen()) {
@@ -45,10 +39,10 @@ struct robot_arm {
     auto const result = std::stoi(serial_.readline());
     if (result == 1) {
       return bin_occupancy::OCCUPIED;
-    } 
+    }
     return bin_occupancy::EMPTY;
   }
-  
+
   serial::Serial serial_;
 };
 
@@ -62,7 +56,8 @@ struct bounds_checked_layout_policy {
         if (((idxs < 0 || idxs > this->extents().extent(Is)) || ...)) {
           throw std::out_of_range("Invalid bin index");
         }
-      }(std::make_index_sequence<sizeof...(idxs)>{});
+      }
+      (std::make_index_sequence<sizeof...(idxs)>{});
       return this->base_t::operator()(idxs...);
     }
   };
@@ -74,22 +69,24 @@ struct bin_checker {
   using data_handle_type = robot_arm*;
 
   reference access(data_handle_type ptr, std::ptrdiff_t offset) const {
-    // We know ptr will be valid asynchronously because we construct it on the stack
-    // of main. In real code we might want to use a shared_ptr or something
-    return std::async([=]{
-      return bin_state{ptr->is_bin_occupied(static_cast<int>(offset)), offset_to_coord(static_cast<int>(offset))};
+    // We know ptr will be valid asynchronously because we construct it on the
+    // stack of main. In real code we might want to use a shared_ptr or
+    // something
+    return std::async([=] {
+      return bin_state{ptr->is_bin_occupied(static_cast<int>(offset)),
+                       offset_to_coord(static_cast<int>(offset))};
     });
   }
 };
 
-using bin_view = stdex::mdspan<bin_state, 
-  // We're treating our 6 bins as a 3x2 matrix
-  stdex::extents<uint32_t, 3, 2>,
-  // Our layout should do bounds-checking
-  bounds_checked_layout_policy,
-  // Our accessor should tell the robot to asynchronously access the bin
-  bin_checker
->;
+using bin_view = stdex::mdspan<bin_state,
+                               // We're treating our 6 bins as a 3x2 matrix
+                               stdex::extents<uint32_t, 3, 2>,
+                               // Our layout should do bounds-checking
+                               bounds_checked_layout_policy,
+                               // Our accessor should tell the robot to
+                               // asynchronously access the bin
+                               bin_checker>;
 
 auto print_state = [](bin_state const& state) {
   std::cout << "Bin at (" << state.coordinate.x << ", " << state.coordinate.y << ") is ";
@@ -107,18 +104,18 @@ auto print_state = [](bin_state const& state) {
 int main() {
   auto arm = robot_arm{"/dev/ttyACM0", 9600};
   auto bins = bin_view(&arm);
-  while(true) {
+  while (true) {
     std::vector<std::future<bin_state>> futures;
     for (auto i = 0u; i < bins.extent(0); ++i) {
       for (auto j = 0u; j < bins.extent(1); ++j) {
-          futures.push_back(bins(i, j));
+        futures.push_back(bins(i, j));
       }
     }
     for (auto const& future : futures) {
-        future.wait();
+      future.wait();
     }
     for (auto& future : futures) {
-        print_state(future.get());
+      print_state(future.get());
     }
     std::cout << "====================" << std::endl;
   }
--- test/test_tooly.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-#include "tooly/tooly.hpp"
-
-TEST(TransformRange, Window) {
-  EXPECT_TRUE(true);
-}
